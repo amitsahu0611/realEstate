16cc2dfc2fe7429e2489f38cbbd0fb5228681612
Amit <sahuamit00786@gmail.com>
2023-12-30 18:26:20 +0530

    added bcrypt for password security

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,13 @@
 import User from "../models/User.js"
+import bcryptjs from 'bcryptjs'
 
 function authController(){
     return {
         async signUp(req,res){
             const{username, email, password} = req.body
+            const hashedPassword = bcryptjs.hashSync(password,10)
 
-            const newUser = new User({username,email,password})
+            const newUser = new User({username,email,password:hashedPassword})
             await newUser.save()
             res.status(200).json({message:"user created successfully"})
         }
