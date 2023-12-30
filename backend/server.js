79ddeb7f14e5bd44d82f2e2aae9e02bd516648b0
Amit <sahuamit00786@gmail.com>
2023-12-30 13:41:25 +0530

    added the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to mongoDB database')
+}).catch((err)=>{
+    console.log(err)
+})
 
 const PORT = 3000||process.env.PORT
 
 app.listen(PORT,()=>{
     console.log(`server is running at PORT ${PORT}`)
 })
+
