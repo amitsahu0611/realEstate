786b99f593e5d4450f1d22dfdd68c6e7f7d797f4
Amit <sahuamit00786@gmail.com>
2023-12-30 19:15:35 +0530

    created the middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,20 @@
 import User from "../models/User.js"
 import bcryptjs from 'bcryptjs'
+import { errorhandler } from "../utils/error.js"
 
 function authController(){
     return {
-        async signUp(req,res){
+        async signUp(req,res,next){
             const{username, email, password} = req.body
             const hashedPassword = bcryptjs.hashSync(password,10)
 
             const newUser = new User({username,email,password:hashedPassword})
-            await newUser.save()
-            res.status(200).json({message:"user created successfully"})
+            try {
+                await newUser.save()
+                res.status(200).json({message:"user created successfully"})
+            } catch (error) {
+                next(error)
+            }
         }
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -21,3 +21,12 @@ app.listen(PORT,()=>{
     console.log(`server is running at PORT ${PORT}`)
 })
 
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode||500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
